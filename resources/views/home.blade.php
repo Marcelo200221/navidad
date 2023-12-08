967a08f9c22fb9b1a3f8bfbc19d65f509fb60900
Marcelo200221 <ma.darras@duocuc.cl>
2023-12-08 16:13:46 -0300

    Inicio Regalo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.general')
+@section('content')
+<div class="contenido">
+    <h1>FELIZ NAVIDAD</h1>
+    <p>Este es mi regalo de navidad para ustedes, para verlo</p>
+    <button class="btn btn-danger">Hagan Click aquí</button>
+</div>
+    
---
+++ resources/views/seleccion.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.general')
+@section('content')
+<div class="contenido">
+
+</div>
